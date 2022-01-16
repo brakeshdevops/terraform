0a6398ee34b9c14ec3b175446ce20c4ef2b35ec9
brakesh <bandarupallirakesh@gmail.com>
2022-01-16 20:33:09 +0530

    Shared library updated

--- 03-resources/resource.tf
+++ 03-resources/resource.tf
@@ -15,33 +15,9 @@ provider "aws" {
 resource "aws_instance" "sample" {
   ami = "ami-04a65ae1c3772365a"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 output "out" {
   value = aws_instance.sample
-}
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
 }
 
 
