c4e1e6723e7dd27d5634a51fb193adb66f0b070b
brakesh <bandarupallirakesh@gmail.com>
2022-01-16 18:10:01 +0530

    Shared library updated

--- 03-resources/resource.tf
+++ 03-resources/resource.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_instance" "sample" {
   ami = "ami-04a65ae1c3772365a"
   instance_type = "t2.micro"
